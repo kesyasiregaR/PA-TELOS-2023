68e804692b7bcdc08dcbfecab24774bb0b7ac1a3
srirahmadani03 <sri19si@mahasiswa.pcr.ac.id>
2023-05-08 11:44:48 +0700

    header ok

--- application/views/user/header.php
+++ application/views/user/header.php
@@ -42,7 +42,7 @@
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
-            
+
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
                 <a class="nav-link" href="<?= site_url('User/Home'); ?>">
@@ -139,7 +139,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= ucwords($this->session->userdata('nama_lengkap')) ?></span>
-                                <img class="img-profile rounded-circle" src="<?= base_url('assets/img/'); ?>user.png">
+                                <img class="img-profile rounded-circle" src="<?= base_url('assets2/img/'); ?>user.png">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
